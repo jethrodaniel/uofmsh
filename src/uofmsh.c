84bfa22771c4ffe29074eb81e9a117a71cf6b692
jethrodaniel <jethrodaniel@gmail.com>
2018-09-23 09:32:54 -0500

    Seperate main loop into a function

--- src/uofmsh.c
+++ src/uofmsh.c
@@ -9,26 +9,43 @@
 #define PROMPT            "uofmsh> "
 #define WHITESPACE_DELIMS " \t"
 
-int main()
+int shell_loop()
 {
   // The line to be read from input
   char line[MAX_LINE_LENGTH];
 
-  bool newline_only = false;
+  // Read commands until the shell is exited via exit or an interrupt
+  while (true) {
+      // Print the prompt before user input
+      printf("%s", PROMPT);
 
-  while (!newline_only) {
-    printf("%s", PROMPT);
+      // Use fgets to save the next line of input in line
+      fgets(line, sizeof line, stdin);
 
-    // Use fgets to save the next line of input in line
-    fgets(line, sizeof line, stdin);
+      // Split the current line by whitespace characters, and save in tokens
+      char tokens[255][MAX_LINE_LENGTH];
 
-    // Exit if command is exit
-    if (strcmp("exit\n", line) == 0) {
-      printf("exit");
-      newline_only = true;
-    }
+      char *token = strtok(line, WHITESPACE_DELIMS);
+      for (int i = 0; token != NULL; i++) {
+        strcpy(tokens[i], token);
+        token = strtok(NULL, WHITESPACE_DELIMS);
+      }
 
+      for (int i = 0; tokens[i][0] != '\0'; i++) {
+        // printf("%i: %s ", i, tokens[i]);
+      }
+
+      // Break out of input loop if command is exit
+      if (strcmp("exit\n", line) == 0) {
+        printf("exit");
+        break;
+      }
   }
 
   return EXIT_SUCCESS;
+}
+
+int main()
+{
+  shell_loop();
 }
