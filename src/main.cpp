b97b55c153f80d712c04633b55eae08410898e57
jethrodaniel <jethrodaniel@gmail.com>
2018-09-29 21:31:08 -0500

    Edit main

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 // Entry point for our shell
 int main(int argc, char **argv) {
   // To avoid the unused params warning from gcc, we reference the parmaeter
-  char *program_name = argv[argc - argc];
+  char *program_name = argv[argc];
 
   // To avoid the unused variable warning from gcc, we use the variable
   (void)program_name;
