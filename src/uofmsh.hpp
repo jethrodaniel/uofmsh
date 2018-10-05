9ce74753744c8fad7cb9e9019651367870de0385
jethrodaniel <jethrodaniel@gmail.com>
2018-10-05 02:30:43 -0500

    Add token and scanner classes

--- src/uofmsh.hpp
+++ src/uofmsh.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <iterator>
 #include <string>
+#include <sstream>
 #include <vector>
 
 #include "helpers.hpp"
@@ -21,10 +22,10 @@ class Shell {
   static void runPrompt();
   static void run(std::string source);
 
-  static void error(int line, std::string message);
   static void report(int line, std::string where, std::string message);
 
   public:
+    static void error(int line, std::string message);
     static int main(int argc, char **argv);
     static bool hadError;
 
@@ -35,7 +36,7 @@ class Shell {
       LEFT_BRACE,    // {
       RIGHT_BRACE,   // }
       PIPE,          // |
-      SEMICOLON,     // ;
+      SEMI,          // ;
       POUND,         // #
 
       // One or two character tokens
@@ -64,6 +65,73 @@ class Shell {
     };
 };
 
+class Token {
+  const Shell::TokenType type;
+  const std::string lexeme;
+  const int line;
+
+public:
+  Token(Shell::TokenType type, std::string lexeme, const int line)
+  : type(type), lexeme(lexeme), line(line) { }
+
+  std::string toString() {
+    // return std::to_string(this->type) + " " + std::to_string(this->lexeme);
+    return this->lexeme;
+  }
+
+};
+
+class Scanner {
+  const std::string source;
+  std::vector<Token> tokens;
+
+  unsigned int start   = 0,
+               current = 0,
+               line    = 1;
+
+  bool isAtEnd() {
+    return current >= source.size();
+  }
+
+  char advance() {
+    current++;
+    return source[current - 1];
+  }
+
+  void addToken(Shell::TokenType type) {
+    std::string text = source.substr(start, current);
+    tokens.push_back(Token(type, text, line));
+  }
+
+  void scanToken() {
+    char c = advance();
+    switch (c) {
+      case '(': addToken(Shell::TokenType::LEFT_PAREN); break;
+      case ')': addToken(Shell::TokenType::RIGHT_PAREN); break;
+      case '{': addToken(Shell::TokenType::LEFT_BRACE); break;
+      case '}': addToken(Shell::TokenType::RIGHT_BRACE); break;
+      case '|': addToken(Shell::TokenType::PIPE); break;
+      case ';': addToken(Shell::TokenType::SEMI); break;
+      case '#': addToken(Shell::TokenType::POUND); break;
+      default:
+        Shell::error(line, "Unexpected character"); break;
+    }
+  }
+
+public:
+  Scanner(std::string source) : source(source) { }
+
+  std::vector<Token> scanTokens() {
+    while (!isAtEnd()) {
+      // at the beginning of the next lexeme
+      start = current;
+      scanToken();
+    }
+
+    tokens.push_back(Token(Shell::TokenType::END, "", line));
+    return tokens;
+  }
+};
 
 } // namespace uofmsh
 
