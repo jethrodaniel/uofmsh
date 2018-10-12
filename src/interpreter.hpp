b46c5d84352c2bc248841628e3c400bccc06efff
jethrodaniel <jethrodaniel@gmail.com>
2018-10-11 22:06:43 -0500

    Add die() function

--- src/interpreter.hpp
+++ src/interpreter.hpp
@@ -19,9 +19,39 @@ class Interpreter {
   static const auto PIPE_READ  = 0,
                     PIPE_WRITE = 1;
 
+  // Exits a process on error
+  void die(std::string msg) const {
+    perror(msg.c_str());
+    _exit(EXIT_FAILURE);
+  }
+
 public:
   // @return  A new interpreter instance
   explicit Interpreter() { }
+
+  // void interpretPipeline(Pipeline p) {
+  //   // Each pipe is represented by two file descripters
+  //   std::vector<int[2]> pipes(p.getCommands().size() - 1);
+
+  //   for (unsigned int i = 0; i < p.getCommands().size(); i++) {
+  //     // Connect the stdout of each command to the stdin of the next command,
+  //     // except for the last comand
+
+  //     if (i == p.getCommands().size() - 1) {
+  //       // last command in this pipeline
+  //       // std::cout << "last command in pipeline " << i << ": " << pipelines[i].getCommands()[j];
+  //     } else {
+  //       // not the last command
+  //
+  //       // Create the pipe
+  //       if (pipe(pipes[i]) == -1) {
+  //         auto error = "Pipe " + std::to_string(i) + " failed. Exiting.";
+  //         perror(error.c_str());
+  //         exit(1);
+  //       }
+  //     }
+  //   }
+  // }
 
   // Runs a simple command without any redirection
   void interpretCommand(Command c) {
@@ -32,29 +62,20 @@ public:
     for (unsigned int i = 0; i < elements.size(); i++)
       args[i] = strdup(elements[i].getLexeme().c_str());
 
-    args[elements.size()] = NULL;
+    args[elements.size()] = NULL; // exec requires a NULL terminator
 
-    pid_t pid; // Hold the process id of the command to execute
+    pid_t pid;  // Hold the process id of the command to execute
+    int status; // The status, updated by wait
 
-    int status;
-
-    if ((pid = fork()) == -1) {
-      perror("Bad fork. Exiting.");
-      exit(1);
-    } else if (pid == 0) {
-      // Run the command as a child process
+    // Run the command as a child process
+    if ((pid = fork()) == -1)
+      die("Bad fork. Exiting.");
+    else if (pid == 0) {
       execvp(args[0], args);
-
-      perror("Bad exec. Exiting.");
-      exit(1);
-    } else {
-      if (wait(&status) < 0) {
-        perror("Bad wait. Exiting.");
-        exit(1);
-      }
-      // Parent finished
-    }
-
+      die("Bad exec. Exiting.");
+    } else
+      if (wait(&status) < 0)
+        die("Bad wait. Exiting.");
   }
 
   // Interpret and run the AST
@@ -63,6 +84,9 @@ public:
 
     if (pipelines.size() == 0)
       return;
+
+    // for (auto p : pipelines)
+    //   interpretPipeline(p);
 
     interpretCommand(p.getPipelines()[0].getCommands()[0]);
   }
